da4c9a2ba9966c126f53abb9ebb9ee67145e7ace
João Ferreira <joaovtf39@gmail.com>
2024-02-24 19:52:49 -0300

    db: add pokemon model

---
+++ src/models/PokemonModel.ts
@@ -0,0 +1,32 @@
+import { Model } from 'sequelize';
+
+export class PokemonModel extends Model {
+  declare id: number;
+  declare tipo: string;
+  declare treinador: string;
+  declare nivel: number;
+  declare readonly created_at: Date;
+  declare readonly updated_at: Date;
+  declare readonly deleted_at?: Date;
+
+  static notHistory: boolean;
+}
+
+module.exports = (sequelize: any, DataTypes: any) => {
+  PokemonModel.init(
+    {
+      tipo: DataTypes.STRING,
+      treinador: DataTypes.STRING,
+      nivel: DataTypes.INTEGER,
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      modelName: 'pokemons'
+    }
+  );
+
+  PokemonModel.notHistory = true;
+
+  return PokemonModel;
+};
