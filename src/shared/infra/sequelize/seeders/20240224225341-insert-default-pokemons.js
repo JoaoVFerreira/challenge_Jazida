56aedaa1036ff9ab1625a69719cd091a5f443a53
João Ferreira <joaovtf39@gmail.com>
2024-02-24 20:15:04 -0300

    db: add seed to populate database

---
+++ src/shared/infra/sequelize/seeders/20240224225341-insert-default-pokemons.js
@@ -0,0 +1,31 @@
+'use strict';
+
+// Populate pokemons table
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.bulkInsert('pokemons', pokemons, { transaction });
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      await queryInterface.bulkDelete('pokemons', { tipo: pokemons.map(({ tipo }) => tipo) }, { transaction });
+    });
+  }
+};
+
+const pokemons = [
+  { tipo: "Pikachu", treinador: "Thiago", nivel: 1, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Bulbasaur", treinador: "Ash", nivel: 1, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Charmander", treinador: "Misty", nivel: 1, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Squirtle", treinador: "Brock", nivel: 1, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Jigglypuff", treinador: "Jessie", nivel: 4, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Snorlax", treinador: "Gary", nivel: 9, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Blastoise", treinador: "Serena", nivel: 6, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Mewtwo", treinador: "Red", nivel: 10, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Gyarados", treinador: "Lance", nivel: 8, created_at: new Date(), updated_at: new Date() },
+  { tipo: "Machop", treinador: "Bruno", nivel: 2, created_at: new Date(), updated_at: new Date() }
+];
