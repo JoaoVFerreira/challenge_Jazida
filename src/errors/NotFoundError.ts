3e2d564c187f18a7f3f70c0666dae8b882bd5c2b
João Ferreira <joaovtf39@gmail.com>
2024-02-26 00:29:49 -0300

    feat: add notfound error

---
+++ src/errors/NotFoundError.ts
@@ -0,0 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
+
+export class NotFoundError extends Error {
+  statusCode: StatusCodes;
+  constructor(message?: string) {
+    super(message);
+    this.statusCode = StatusCodes.NOT_FOUND;
+  }
+}
