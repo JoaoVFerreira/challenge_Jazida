1763b0c406932458db558291c4fc7c00404a14a1
João Ferreira <joaovtf39@gmail.com>
2024-02-27 17:51:48 -0300

    test: add battlePokemon unit tests

--- __tests__/unit/modules/pokemons/useCases/BattlePokemonUseCase.test.ts
+++ __tests__/unit/modules/pokemons/useCases/BattlePokemonUseCase.test.ts
@@ -1,5 +1,6 @@
 import { BattlePokemonUseCase } from '$modules/pokemons/application/useCases/BattlePokemonUseCase';
 import { PokemonsRepositoryInMemory } from '$test/unit/modules/pokemons/inMemory/PokemonsRepositoryInMemory';
+import { NotFoundError } from '$errors/NotFoundError';
 
 const charizard = { trainer: 'Less', level: 1, type: 'Charizard' };
 const articuno = { trainer: 'Sacy', level: 7, type: 'Articuno' };
@@ -19,8 +20,50 @@ describe('BattlePokemonUseCase', () => {
     expect(result.perdedor).toBeTruthy();
     expect(result.vencedor).toBeTruthy();
   });
-});
 
+  it('Should increment winner and decrement loser level', async () => {
+    // ARRANGE
+    const { pokemonsRepository, sut } = makeSut();
+    const pokemonA = await pokemonsRepository.create(exodia);
+    const pokemonB = await pokemonsRepository.create(charizard);
+
+    // ACT
+    const result = await sut.execute({ pokemonIdA: pokemonA.id, pokemonIdB: pokemonB.id });
+
+    // ASSERT
+    expect(result.vencedor.nivel).toBe(pokemonA.level + 1);
+    expect(result.perdedor.nivel).toBe(pokemonB.level - 1);
+  });
+
+  it('Should delete when the loser reaches level 0', async () => {
+    // ARRANGE
+    const { pokemonsRepository, sut } = makeSut();
+    const pokemonA = await pokemonsRepository.create(exodia);
+    const pokemonB = await pokemonsRepository.create(charizard);
+
+    // ACT
+    await sut.execute({ pokemonIdA: pokemonA.id, pokemonIdB: pokemonB.id });
+    const result = await pokemonsRepository.getOne({ id: pokemonB.id });
+    
+    // ASSERT
+    expect(result).toBeFalsy();
+  });
+
+  it('Should throw when one of the given pokemons is not found', async () => {
+    // ARRANGE
+    const { pokemonsRepository, sut } = makeSut();
+    const pokemonA = await pokemonsRepository.create(exodia);
+    const inexistentPokemon = { id: 999, trainer: 'Jhon', level: 7, type: 'Mewtwo404' };
+
+    // ACT
+    const result = sut.execute({ pokemonIdA: pokemonA.id, pokemonIdB: inexistentPokemon.id });
+
+    // ASSERT
+    await expect(result).rejects.toThrow(
+      new NotFoundError(BattlePokemonUseCase.Messages.pokemonsNotFound)
+    );
+  });
+});
 
 type sutTypes = {
   sut: BattlePokemonUseCase;
